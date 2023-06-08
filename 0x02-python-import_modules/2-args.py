7745e32d0dfff1951a9741e524183764120e2299
Kariaki58 <kariakichernocherno@gmail.com>
2023-06-08 12:05:04 -0700

    forget to add . at the end when no argument is given

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     args_item = sys.argv
     length = len(args_item) - 1
     if length == 0:
-        print("{} arguments:".format(length))
+        print("{} arguments.".format(length))
     elif length == 1:
         print("{} argument:".format(length))
         print("{}: {}".format(length, args_item[1]))
