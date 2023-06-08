5864581815c6462a961517d833e4dec790ffa1bf
Kariaki58 <kariakichernocherno@gmail.com>
2023-06-08 12:18:33 -0700

    program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import sys
-
-
-args = sys.argv
-convertToInt = 0
-for i in range(1, len(args)):
-	convertToInt += int(args[i])
-print("{}".format(converToInt))
+if __name__=="__main__":
+    import sys
+    args = sys.argv
+    convertToInt = 0
+    for i in range(1, len(args)):
+        convertToInt += int(args[i])
+    print("{}".format(convertToInt))
