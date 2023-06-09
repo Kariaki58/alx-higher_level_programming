907b4a6ac8a26366942e1ae973d059271c11c5d1
Kariaki58 <kariakichernocherno@gmail.com>
2023-06-09 02:00:16 -0700

    make it indentable

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,1 +1,8 @@
-
+#!/usr/bin/python3
+import dis
+if __name__ == "__main__":
+    from magic_calculation import add, sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
