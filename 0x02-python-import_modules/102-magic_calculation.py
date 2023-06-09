ef7e58f52400cc65a650e4004b43a86316102054
Kariaki58 <kariakichernocherno@gmail.com>
2023-06-09 02:25:21 -0700

    program that check what is magic function is doing using python bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,4 +6,6 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
+        return c
+    return sub(a, b)
 print(dis.dis(magic_calculation))
