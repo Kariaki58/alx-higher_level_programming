9401d240082cdb1b2bd5cbd935f5c5205b862a10
Kariaki58 <kariakichernocherno@gmail.com>
2023-06-09 05:40:11 -0700

    excange conditional statement

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,8 @@
 def no_c(my_string):
     new_str = ""
     for i in range(len(my_string)):
-        if my_string[i] != "c" or my_string[i] != "C":
+        if my_string[i] == "c" or my_string[i] == "C":
+            continue
+        else:
             new_str += my_string[i]
     return new_str
