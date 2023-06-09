ec83e3b375e3614b5d24cd606461e53f8a275a2e
Kariaki58 <kariakichernocherno@gmail.com>
2023-06-09 02:12:43 -0700

    program the does exactl the same as the following python betecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import dis
-if __name__ == "__main__":
-    from magic_calculation import add, sub
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
+print(dis.dis(magic_calculation))
