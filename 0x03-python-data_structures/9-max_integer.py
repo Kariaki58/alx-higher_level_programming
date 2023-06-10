a423d6ca2e85584bb2cc0504fc4bbbc486064bb5
Kariaki58 <kariakichernocherno@gmail.com>
2023-06-10 07:55:10 -0700

    checking if my list is none

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if my_list is None:
         return None
     if len(my_list) == 0:
-        return None
+        return "None"
     max = 0
     for i in my_list:
         if max < i:
