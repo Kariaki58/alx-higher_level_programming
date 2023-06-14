6f253693caad40a07548b2cb07b944b37ac345c6
Kariaki58 <kariakichernocherno@gmail.com>
2023-06-14 05:35:33 -0700

    function that returns a key with the biggest inteer value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    high_score = 0
+    score_owner = None
+    for keys, value in a_dictionary.items():
+        if value > high_score:
+            score_owner = keys
+            high_score = value
+    return score_owner
