766a5c94d35a28ad67ebcecef891c7e0dce60b24
Kariaki58 <kariakichernocherno@gmail.com>
2023-06-14 08:07:20 -0700

    tying to handdle IX in the string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,14 @@ def roman_to_int(roman_string):
     mapper = ["I", "V", "X", "L", "C", "D", "M"]
     values = [1, 5, 10, 50, 100, 500, 1000]
     number = 0
+    counter = 0
     for i in roman_string:
-        counter = mapper.count(i)
-        index_count = mapper.index(i)
-        number = number + (values[index_count] * counter)
+        if (i == "I" or counter) and "IX" in roman_string:
+            counter = 1
+            if i == "X":
+                number = number + 9
+            continue
+        else:
+            index_count = mapper.index(i)
+            number = number + values[index_count]
     return number
