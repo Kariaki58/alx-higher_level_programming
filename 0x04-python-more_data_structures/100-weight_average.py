19328cf8c8ac20fcf88ab67f47d01566bcb3ae13
Kariaki58 <kariakichernocherno@gmail.com>
2023-06-15 05:59:22 -0700

    forget to return 0 when the list is empty

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list is None:
+        return None
+    if len(my_list) == 0:
+        return 0
     sum_tuple = 0
     mult = 0
     for tup in my_list:
