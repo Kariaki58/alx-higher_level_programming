cf705c3d454a08cdf233c003c6ec13851c7b2162
Kariaki58 <Kariakichernocherno@gmail.com>
2023-06-18 05:54:13 +0200

    checking betty

--- 0x00-python-hello_world/checking.c
+++ 0x00-python-hello_world/checking.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - description
- * Return: return 0.
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int a = 5;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-	printf("%d\n", a);
-	printf("Hello world\n");
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
