10025c67ca3c2203a38abe62177a05ace563251b
Kariaki58 <Kariakichernocherno@gmail.com>
2023-06-26 15:59:24 +0100

    write a code that print exactly a byte code on disassemling

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if i > a:
-                raise 'Too far'
+                raise Exception('Too far')
         except:
             pass
 dis.dis(magic_calculation)
