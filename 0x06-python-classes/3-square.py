20a62e87536ce697fdadbfbc80211512947dc9ec
Kariaki58 <Kariakichernocherno@gmail.com>
2023-06-27 15:49:23 +0100

    modified square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,11 +6,13 @@ class Square:
     """define a square with a an instance"""
     
     def __init__(self, size=0):
-        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     def area(self):
+        """return area"""
         return self.__size * self.__size
