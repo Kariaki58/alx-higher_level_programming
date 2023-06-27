a5eb0f0cc0987391a546ffa8cdd480cbfe261693
Kariaki58 <Kariakichernocherno@gmail.com>
2023-06-27 16:37:05 +0100

    added - to the square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""write a class Square that defines a square"""
+
+
+class Square:
+    """drawing a square in the terminal"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value[0]) is not int and type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            length = int(self.area() / self.__size)
+            for i in range(length):
+                for j in range(length):
+                    print("#", end="")
+                print()
