b8779ed18c08c199b1f71a77b6f3b8043e33b65b
Kariaki58 <Kariakichernocherno@gmail.com>
2023-06-27 15:58:23 +0100

    fixing pycodestyle

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,9 +10,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area(self):
-        """return area"""
         return self.__size * self.__size
