b309c2462e3e87e62bcae9d6018d023cca8498aa
Kariaki58 <Kariakichernocherno@gmail.com>
2023-06-28 09:31:33 +0100

    i just wrote bad code if it will code

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,11 +4,7 @@
 
 class Square:
     def __init__(self, size=0, position=(0, 0)):
-        if type(position) is not tuple or len(position) < 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if type(position[0]) is not int or type(position[1]) is not int:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if position[0] < 0 or position[1] < 0:
+        if type(position) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = size
         self.__position = position
