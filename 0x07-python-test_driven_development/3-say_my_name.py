7bea6c3fc21cc6befe9c5b0a366c13c762aa16c5
Kariaki58 <Kariakichernocherno@gmail.com>
2023-07-06 15:12:44 +0100

    function that say my name and all it's doctests

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+say_my_name: log out my name to stdout
+"""
+def say_my_name(first_name, last_name=""):
+    """print out my name
+    Args:
+        first_name (str): get the first name
+        last_name (str, optional): get the last name. Defaults to "".
+
+    Raises:
+        TypeError: if a wrong input is given
+    """
+    message = "first_name must be a string"
+    message2 = "last_name must be a string"
+    if not isinstance(first_name, str):
+        raise TypeError(message)
+    if not isinstance(last_name, str):
+        raise TypeError(message2)
+    print("My name is {} {}".format(first_name, last_name))
