4993432775cd2d2fca60b25963251895553ab80f
Kariaki58 <Kariakichernocherno@gmail.com>
2023-07-06 15:34:26 +0100

    print a square with size n

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""print a square with the character #
+"""
+
+
+def print_square(size):
+    """print a square
+
+    Args:
+        size (int): input int
+
+    Raises:
+        TypeError: wrong input
+        ValueError: wrong input
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
