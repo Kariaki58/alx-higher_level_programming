3e17e7feb320351bd5aceaf842d244055c227669
Kariaki58 <Kariakichernocherno@gmail.com>
2023-07-07 04:54:09 +0100

    added more test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,14 +12,30 @@ class TestMaxInteger(unittest.TestCase):
         unittest (inherit): gotten from unittest module
     """
     def test_list(self):
+        """double test lists and None
+        """
         data = []
         MainInput = [1, 2, 3, 4]
         self.assertEqual(max_integer(data), None)
         self.assertEqual(max_integer(MainInput), 4)
 
+    def test_random(self):
+        """when the list is not arange
+        """
+        data = [1, 4, 3, 2]
+        self.assertEqual(max_integer(data), 4)
+    
+    def test_error(self):
         """check if an exception is raised
         """
-    def test_error(self):
         data1, data2 = 1, 5 + 3j
         self.assertRaises(TypeError, max_integer, data1)
         self.assertRaises(TypeError, max_integer, data2)
+    
+    def test_floatingpoint(self):
+        """test floating point numbers"""
+        numbers = [-3.2, 4.5, 9.3, 8.2]
+        self.assertEqual(max_integer(numbers), 9.3)
+    
+    def test_string(self):
+        self.assertEqual(max_integer(""), None)
