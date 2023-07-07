5d8dccb0b0af085bd97360cdd079301f67502e9a
Kariaki58 <Kariakichernocherno@gmail.com>
2023-07-07 09:08:46 +0100

    adding more mandatory test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -18,7 +18,7 @@ class TestMaxInteger(unittest.TestCase):
         MainInput = [1, 2, 3, 4]
         self.assertEqual(max_integer(data), None)
         self.assertEqual(max_integer(MainInput), 4)
-
+        self.assertEqual(max_integer[7], 7)
     def test_random(self):
         """when the list is not arange
         """
@@ -37,5 +37,11 @@ class TestMaxInteger(unittest.TestCase):
         numbers = [-3.2, 4.5, 9.3, 8.2]
         self.assertEqual(max_integer(numbers), 9.3)
     
+    def test_beginning(self):
+        self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
+        self.assertEqual(max_integer([4, -3, 12]), 12)
+        self.assertEqual(max_integer([-1, -2, -4, -3]), -1)
+
     def test_string(self):
         self.assertEqual(max_integer(""), None)
+        #3, 5, 6, 7
