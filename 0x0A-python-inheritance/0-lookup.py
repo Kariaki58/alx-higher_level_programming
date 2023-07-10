51c95256b7798dd02aad126d8df7eae3997b603c
Kariaki58 <Kariakichernocherno@gmail.com>
2023-07-10 13:15:01 +0100

    loopup function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+
+"""lookup function returns a list of directorys
+"""
 def lookup(obj):
+    """function that returns a list of directorys
+
+    Args:
+        obj (object): get a object from main
+
+    Returns:
+        obj: return a list of directories
+    """
     return dir(obj)
