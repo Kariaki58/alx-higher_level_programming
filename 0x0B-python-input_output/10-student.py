7c307eaa777c1c4d5d8acc1ddcbe4d9c844952cc
Kariaki58 <Kariakichernocherno@gmail.com>
2023-07-11 15:58:54 +0100

    tructated the line to be less than 80

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""student class"""
+
+
+class Student:
+    """instance atributes
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """
+    convert to json
+    """
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            for data in attrs:
+                if type(data) is not str:
+                    continue
+                data = {d: getattr(self, d) for d in attrs if hasattr(self, d)}
+                return data
+        return self.__dict__
