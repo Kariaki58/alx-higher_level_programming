e3345fb744828aea3d8020f4433a8ba648930c83
Kariaki58 <Kariakichernocherno@gmail.com>
2023-07-11 15:37:52 +0100

    fixed pycodestyle

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,10 +9,9 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     """
     convert to json
     """
     def to_json(self):
         return self.__dict__
-    
