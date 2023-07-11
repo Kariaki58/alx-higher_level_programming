2a8123ab251bd7e5ccdc4dd17419fc90dfed8861
Kariaki58 <Kariakichernocherno@gmail.com>
2023-07-11 12:13:04 +0100

    added README.md file and also functiont that reads a text file (UTF-8) and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""read from a file
+"""
+
+
+def read_file(filename=""):
+    """read from a text file(UTF-8) and prints it to stdout
+
+    Args:
+        filename (str, optional): get the file name. Defaults to "".
+    """
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read())
