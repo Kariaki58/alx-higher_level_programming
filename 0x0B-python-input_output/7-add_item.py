b406d8056222c39352a31b96c4123137bd661cf8
Kariaki58 <Kariakichernocherno@gmail.com>
2023-07-11 15:08:32 +0100

    function that creates an Object from a JSON file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """script that adds all arguments to a python list"""
 import sys
-Python_list_data = __import__('5-save_to_json_file').save_to_json_file
-json_list = __import__('6-load_from_json_file').load_from_json_file
+
 
 
 if __name__ == "__main__":
+    Python_list_data = __import__('5-save_to_json_file').save_to_json_file
+    json_list = __import__('6-load_from_json_file').load_from_json_file
+
     try:
         Python_list = json_list("add_item.json")
     except FileNotFoundError:
