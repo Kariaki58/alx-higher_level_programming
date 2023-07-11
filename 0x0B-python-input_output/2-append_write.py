368a0b98aa73f4f1bc0d479644c9c9dcd9bcde90
Kariaki58 <Kariakichernocherno@gmail.com>
2023-07-11 13:08:21 +0100

    fix pycodestyle documentation

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,6 +3,15 @@
 
 
 def append_write(filename="", text=""):
+    """append to a file
+
+    Args:
+        filename (str, optional): file name input. Defaults to "".
+        text (str, optional): main text. Defaults to "".
+
+    Returns:
+        int: returnt the number of characters added
+    """
     char_len = 0
 
     with open(filename, mode="a", encoding="UTF8") as file:
