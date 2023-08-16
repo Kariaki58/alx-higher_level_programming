846ef260e050a7ce991baeaf544774623d3bc590
Kariaki58 <kariakichernocherno@gmail.com>
2023-08-16 20:21:30 +0100

    bug fixed remove ; from a line

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- script that lists all shows contained in the database
 SELECT tvs.title, tvg.genre_id FROM tv_shows AS tvs
 	LEFT OUTER JOIN tv_show_genres AS tvg
-	ON tvs.id = tvg.show_id;
-	ORDER BY tvs.title, tvg.genre_id
+	ON tvs.id = tvg.show_id
+	ORDER BY tvs.title, tvg.genre_id ASC;
