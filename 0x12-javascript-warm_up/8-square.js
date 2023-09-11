261ff224e23c43ba73cd8959d40dff4154499532
Kariaki58 <programmerjavascript901@gmail.com>
2023-09-11 15:16:40 +0100

    prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+array = process.argv;
+if (array.length === 2 || isNaN(array[2])) {
+    console.log("Missing size");
+}
+for (let i = 0; i < parseInt(array[2]); i++) {
+    let string = '';
+    for (let j = 0; j < parseInt(array[2]); j++) {
+        string += 'X';
+    }
+    console.log(string);
+}
