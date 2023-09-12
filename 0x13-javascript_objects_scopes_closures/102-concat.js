89e48bb0a2bf67b247dad1baa28b1b3139cc1373
Kariaki58 <programmerjavascript901@gmail.com>
2023-09-12 22:01:00 +0100

    better solution

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,28 +1,7 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-const args = process.argv.slice(2);
-const file1 = args[0];
-const file2 = args[1];
-const destinationFile = args[2];
-
-fs.readFile(file1, 'utf8', (err, data1) => {
-  if (err) {
-    console.error(`Error reading ${file1}: ${err}`);
-    process.exit(1); 
-  }
-  fs.readFile(file2, 'utf8', (err, data2) => {
-    if (err) {
-      console.error(`Error reading ${file2}: ${err}`);
-      process.exit(1); 
-    }
-    const concatenatedData = data1 + data2;
-    fs.writeFile(destinationFile, concatenatedData, 'utf8', (err) => {
-      if (err) {
-        console.error(`Error writing to ${destinationFile}: ${err}`);
-        process.exit(1); 
-      }
-      console.log(`Concatenated ${file1} and ${file2} to ${destinationFile}`);
-    });
-  });
-});
+let args = process.argv.slice(2);
+let fs = require('fs');
+let file1 = fs.readFileSync(`./${args[0]}`);
+let file2 = fs.readFileSync(`./${args[1]}`);
+fs.writeFileSync(`./${args[2]}`, file1 + file2);
