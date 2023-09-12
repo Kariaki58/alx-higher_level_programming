8330c35f56910410ff659e742b0860973defc98f
Kariaki58 <programmerjavascript901@gmail.com>
2023-09-12 20:33:42 +0100

    creates a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return this;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let sqr = '';
+      for (let j = 0; j < this.width; j++) {
+        sqr += 'X';
+      }
+      console.log(sqr);
+    }
+  }
+}
+module.exports = Rectangle;
