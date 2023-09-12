27228ccdd6f0209423fe024d28b9b64672ac0297
Kariaki58 <programmerjavascript901@gmail.com>
2023-09-12 20:41:40 +0100

    fixed js style

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,28 +1,30 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-        return this;
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return this;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let sqr = '';
+      for (let j = 0; j < this.width; j++) {
+        sqr += 'X';
       }
-      this.width = w;
-      this.height = h;
+      console.log(sqr);
     }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        let sqr = '';
-        for (let j = 0; j < this.width; j++) {
-          sqr += 'X';
-        }
-        console.log(sqr);
-      }
-    }
-    rotate () {
-        [this.height, this.width] = [this.width, this.height];
-    }
-    double () {
-        this.width *= 2;
-        this.height *= 2;
-    }
+  }
+
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 module.exports = Rectangle;
