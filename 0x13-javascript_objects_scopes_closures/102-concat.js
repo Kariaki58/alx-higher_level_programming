d258e1a6e63039e57f6ea2a278d44dcd696440eb
Kariaki58 <programmerjavascript901@gmail.com>
2023-09-12 22:01:27 +0100

    finish for the day

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let args = process.argv.slice(2);
-let fs = require('fs');
-let file1 = fs.readFileSync(`./${args[0]}`);
-let file2 = fs.readFileSync(`./${args[1]}`);
+const args = process.argv.slice(2);
+const fs = require('fs');
+const file1 = fs.readFileSync(`./${args[0]}`);
+const file2 = fs.readFileSync(`./${args[1]}`);
 fs.writeFileSync(`./${args[2]}`, file1 + file2);
