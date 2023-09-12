98fa3addf842c14ac114d9f92f3d64e6de6f062f
Kariaki58 <programmerjavascript901@gmail.com>
2023-09-12 20:54:46 +0100

    fixed semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,18 +2,18 @@
 
 const othersquare = require('./5-square');
 class Square extends othersquare {
-    charPrint (c) {
-        let char = c;
-        if (c === undefined) {
-            char = 'X';
-        }
-        for (let i = 0; i < this.height; i++) {
-            let sqr = '';
-            for (let j = 0; j < this.width; j++) {
-                sqr += char;
-            }
-            console.log(sqr);
-        }
+  charPrint (c) {
+    let char = c;
+    if (c === undefined) {
+      char = 'X';
     }
+    for (let i = 0; i < this.height; i++) {
+      let sqr = '';
+      for (let j = 0; j < this.width; j++) {
+        sqr += char;
+      }
+      console.log(sqr);
+    }
+  }
 }
 module.exports = Square;
