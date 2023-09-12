ddc1f3a16ce16042999633b688c19681fba340d3
Kariaki58 <programmerjavascript901@gmail.com>
2023-09-12 21:44:41 +0100

    sorte occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
+
 const dict = require('./101-data.js').dict;
-
 const newDict = {};
-
 Object.getOwnPropertyNames(dict).forEach(data => {
   if (newDict[dict[data]] === undefined) {
     newDict[dict[data]] = [data];
