39cf0e616e894af273bbf13148a5f4c6015c12bc
Kariaki58 <programmerjavascript901@gmail.com>
2023-09-16 11:46:01 +0100

    made order ascending

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
             user=argv[1], passwd=argv[2], database=argv[3]
             )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY ASC")
     status = cursor.fetchall()
     for data in status:
         print(data)
