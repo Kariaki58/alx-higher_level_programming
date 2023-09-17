3d9becaeacf062eb3db56f26e81babcfa04e086f
Kariaki58 <programmerjavascript901@gmail.com>
2023-09-17 04:51:49 +0100

    update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""import modules"""
+from sqlalchemy import create_engine
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+            )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    qury = session.query(State).filter(State.id == 2).first()
+    qury.name = "New Mexico"
+    session.commit()
+    session.close()
