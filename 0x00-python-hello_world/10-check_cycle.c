662da361fd190e9ee7687dfef5c57cd2b2cce06f
Kariaki58 <programmerjavascript901@gmail.com>
2023-09-23 19:07:22 +0100

    check if it is a cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,8 +10,7 @@ int check_cycle(listint_t *list)
 {
 	listint_t *tortoise = list;
 	listint_t *hare = list->next;
-
-	if (!tortoise || !hare || !(list->next))
+	if (!list)
 		return (0);
 	while (hare && hare->next)
 	{
