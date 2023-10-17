12e7783bc2a772ca80395a0ee4b1a16f3240dc28
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-17 21:07:37 +0100

    trying next solution

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- script that creates a table
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table VALUES 
+INSERT INTO second_table(id, name, score) VALUES 
 (1, "Jonh", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
