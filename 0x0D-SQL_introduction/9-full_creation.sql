e9153b42dafa2ebb76fc534d5c28017636fe093b
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-17 20:54:01 +0100

    ensuring script do not fail if database exits

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,8 @@
 -- script that creates a table
-CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table VALUES 
 (1, "Jonh", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
 (4, "George", 8);
+SELECT * FROM second_table;
