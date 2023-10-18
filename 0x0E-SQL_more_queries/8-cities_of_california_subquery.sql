e9ae07eb4465a3d259855af1ce7881cf47b6f4d7
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-18 16:20:23 +0100

    order acending order

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- script that lists all cities of California that can be found in the database.
 SELECT id, name FROM cities WHERE id=(
-	SELECT id FROM states WHERE name="California") ORDER BY cities.id;
+	SELECT id FROM states WHERE name="California") ORDER BY cities.id ASC;
