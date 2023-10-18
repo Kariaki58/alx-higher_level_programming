eb638b8f5a252429f3490e058bdd96bbcfe04d52
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-18 01:31:35 +0100

    say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records that has a name
-SELECT score, name FROM second_table WHERE name != "";
+SELECT score, name FROM second_table WHERE name != "" OR name = NULL ORDER BY score DESC;
