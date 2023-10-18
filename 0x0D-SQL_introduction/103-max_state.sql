ba361409011b04f71dc3b6e5604f4f4f8bb36d55
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-18 03:00:36 +0100

    order by state asc

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- Temperatures #2
 USE hbtn_0c_0;
-SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY max_temp;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
