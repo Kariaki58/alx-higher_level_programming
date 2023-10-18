bc12f062d8ccfd2ef1d3b5c0e6cb472e3a07fbc8
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-18 01:25:47 +0100

    task 15-16

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- Number of score
+SELECT score, COUNT(score) AS `number` FROM second_table GROUP BY score;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- lists all records that has a name
+SELECT score, name FROM second_table WHERE name != "";
