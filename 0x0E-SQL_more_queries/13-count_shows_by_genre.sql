9cdf11200f8924490ff8f6f0bdc875e97b22fcc7
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-19 11:09:11 +0100

    modifield solution

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Number of shows by genre
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id)
 AS number_of_shows FROM tv_genres 
-JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY genre ORDER BY number_of_shows DESC;
