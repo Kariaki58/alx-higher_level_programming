f5166f371ea819f9cdd4bb1cbd40f9ab5e45953b
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-13 13:32:02 +0100

    finding second biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,15 +3,8 @@
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  const sortArray = process.argv.slice(2);
-  const array = sortArray.sort();
-  for (let i = 0; i < array.length; i++) {
-    if (i < array.length - 1) {
-      if (array[i] === array[i + 1]) {
-        array.pop();
-      }
-    }
-  }
-  console.log(array);
-  console.log(array[array.length - 2]);
+  const sortArray = process.argv.slice(2, process.argv.length);
+  const array = sortArray.sort((a, b) => b - a);
+  const remove_dup = Array.from(new Set(array));
+  console.log(remove_dup[1]);
 }
