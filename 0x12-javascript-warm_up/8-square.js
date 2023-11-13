ffc2e07891b6562324b9dfc2335cdff9a0bce31a
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-13 12:09:32 +0100

    fixing semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 
 if (process.argv.length === 2 || isNaN(parseInt(process.argv[2], 10))) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else {
-	let number = parseInt(process.argv[2], 10);
-	let squares = ""
-	for (let i = 0; i < number; i++) {
-		for (let j = 0; j < number; j++) {
-			squares += "X"
-		}
-		if (i != number - 1) {
-			squares += "\n"
-		}
-	}
-	if (number > 0) {
-		console.log(squares);
-	}
+  const number = parseInt(process.argv[2], 10);
+  let squares = '';
+  for (let i = 0; i < number; i++) {
+    for (let j = 0; j < number; j++) {
+      squares += 'X';
+    }
+    if (i !== number - 1) {
+      squares += '\n';
+    }
+  }
+  if (number > 0) {
+    console.log(squares);
+  }
 }
