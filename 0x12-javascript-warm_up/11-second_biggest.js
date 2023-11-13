319cd9766f22e95ad0095767be5b09a70b53e2bb
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-13 12:58:42 +0100

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,5 +4,14 @@ if (process.argv.length < 4) {
   console.log(0);
 } else {
   const sortArray = process.argv.slice(2);
-  console.log(parseInt(sortArray.sort()[sortArray.length - 2], 10));
+  const array = sortArray.sort();
+  for (let i = 0; i < array.length; i++) {
+    if (i < array.length - 1) {
+      if (array[i] === array[i + 1]) {
+        array.pop();
+      }
+    }
+  }
+  console.log(array);
+  console.log(array[array.length - 2]);
 }
