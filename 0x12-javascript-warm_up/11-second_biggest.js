970fc1c798b534f2033dc0a9af68af4dc3c9c75a
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-13 12:13:59 +0100

    fixed semistandard in js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 if (process.argv.length < 4) {
-	console.log(0);
+  console.log(0);
 } else {
-	let sortArray = process.argv.slice(2);
-	console.log(parseInt(sortArray.sort()[sortArray.length - 2], 10));
+  const sortArray = process.argv.slice(2);
+  console.log(parseInt(sortArray.sort()[sortArray.length - 2], 10));
 }
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-function add(a, b) {
-	console.log(a + b);
+function add (a, b) {
+  console.log(a + b);
 }
 
-let prams1 = Number(process.argv[2]);
-let prams2 = Number(process.argv[3]);
+const prams1 = Number(process.argv[2]);
+const prams2 = Number(process.argv[3]);
 
 add(prams1, prams2);
