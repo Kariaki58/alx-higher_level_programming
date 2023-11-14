5127cb396e00098d953079975bbd63452148f7a2
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-14 15:02:56 +0100

    square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,48 +1,59 @@
 #!/usr/bin/node
 
 class Rectangle {
-    char = "X";
-    constructor (w, h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    print () {
-        let string = ""
-        for (let i = 0; i < this.height; i++) {
-            for (let j = 0; j < this.width; j++) {
-                string += this.char;
-            }
-            if (i < this.height - 1) {
-                string += "\n"
-            }
-        }
-        console.log(string)
+  }
+
+  print () {
+    let string = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        string += "X";
+      }
+      if (i < this.height - 1) {
+        string += '\n';
+      }
     }
-    rotate () {
-        let temp = this.width;
-        this.width = this.height;
-        this.height = temp;
+    console.log(string);
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  charPrint (c) {
+    let string = '';
+
+    if (c === undefined) {
+      c = "X";
     }
-    double () {
-        this.width *= 2;
-        this.height *= 2;
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        string += c;
+      }
+      if (i < this.height - 1) {
+        string += '\n';
+      }
     }
-    charPrint (c) {
-        if (c === undefined) {
-            c = "X"
-        }
-        this.char = c;
-        this.print();
-    }
+    console.log(string);
+  }
 }
 
-
 class Square extends Rectangle {
-    constructor (size) {
-        super(size, size);
-    }
+  constructor (size) {
+    super(size, size);
+  }
 }
 
 module.exports = Square;
