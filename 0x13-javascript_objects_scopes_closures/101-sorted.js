430c0922c6db79e530ceec4b83f499c3014f2eab
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-14 17:26:40 +0100

    semistandard fixed

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 
-const dict = require("./101-data").dict
-let empty_dict = {}
-
+const dict = require('./101-data').dict;
+const empty_dict = {};
 
 for (const key in dict) {
-	if (empty_dict[dict[key]] !== undefined) {
-		empty_dict[dict[key]].push(key)
-	} else {
-		empty_dict[dict[key]] = [key];
-	}
+  if (empty_dict[dict[key]] !== undefined) {
+    empty_dict[dict[key]].push(key);
+  } else {
+    empty_dict[dict[key]] = [key];
+  }
 }
 
-console.log(empty_dict)
+console.log(empty_dict);
