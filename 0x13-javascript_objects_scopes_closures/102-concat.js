ee68fbd644acffe2332cd27276c72f2919e3daff
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-14 17:54:19 +0100

    concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const fileStorage = require('fs');
+const argv = require('process').argv;
+
+fileStorage.readFile(argv[2], 'utf8', function (err, data) {
+  if (err) {
+    throw err;
+  }
+  fileStorage.readFile(argv[3], 'utf8', function (err, data2) {
+    if (err) {
+        throw err;
+    }
+    fileStorage.writeFile(argv[4], data + data2, err => {
+        if (err) {
+            throw err;
+        }
+    });
+  });
+});
