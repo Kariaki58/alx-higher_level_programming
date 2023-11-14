59872ae6fa6c15aa5201dd02c294057b8d19ac39
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-14 15:23:30 +0100

    number conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-    return function (numb) {
-        return numb.toString(base);
-    };
-}
+  return function (numb) {
+    return numb.toString(base);
+  };
+};
