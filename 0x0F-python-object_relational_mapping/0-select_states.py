79f90f7d995aee30955d81bcbca0f730ad8ee886
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-17 13:51:47 +0100

    get all states from a database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+"""get all states from a state table in database"""
 import MySQLdb
 from sys import argv
 
-connect = MySQLdb.connect(
-    user=argv[1], passwd=argv[2], db=argv[3], host="localhost", port=3306
-    )
 
-cur = connect.cursor()
+if __name__ == "__main__":
+    connect = MySQLdb.connect(
+        user=argv[1], passwd=argv[2], db=argv[3], host="localhost", port=3306
+        )
 
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur = connect.cursor()
 
-rows = cur.fetchall()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
-for row in rows:
-    print(row)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    cur.close()
+    connect.close()
