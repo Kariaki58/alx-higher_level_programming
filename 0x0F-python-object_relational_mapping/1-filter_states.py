490e9c0258724879a7291c829fdbbcc4131ec09f
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-17 14:01:04 +0100

    filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""filter states"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    connect = MySQLdb.connect(
+        user=argv[1], passwd=argv[2], db=argv[3], host="localhost", port=3306
+        )
+
+    cur = connect.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    cur.close()
+    connect.close()
