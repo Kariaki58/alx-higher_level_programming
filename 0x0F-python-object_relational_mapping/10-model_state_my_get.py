2016bccf6146554c11a66d5907bb3aad51aab3f1
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-17 18:36:06 +0100

    geting a state id

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,14 +10,14 @@ if __name__ == "__main__":
     """entry point"""
     engine = create_engine(
         'mysql://{}:{}@localhost:3306/{}'.format(sys.argv[1],
-                                            sys.argv[2],
-                                            sys.argv[3]),
+                                                 sys.argv[2],
+                                                 sys.argv[3]),
         echo=False)
     Base.metadata.create_all(bind=engine)
     Session = sessionmaker(engine)
     session = Session()
     data = sys.argv[4]
-    query = session.query(State).filter(State.name==data).all()
+    query = session.query(State).filter(State.name == data).all()
     if query:
         for data in query:
             print(data.id)
