cee18db7ff612677ce57c0db71257dade759c7c0
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-17 14:46:01 +0100

    using id instead of state id, rm sql injection in task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
         )
 
     cur = connect.cursor()
-
-    cmd = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC"
-    cur.execute(cmd.format(argv[4]))
+    cmd = "SELECT * FROM states WHERE name = %s ORDER BY id"
+    cur.execute(cmd, (argv[4],))
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
     cur.close()
