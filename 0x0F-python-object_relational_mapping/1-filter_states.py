1706e5ff008e3ff6286bfbfe919e2ac8bdb3b610
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-18 20:59:40 +0100

    checking for postion 1 in row

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
     rows = cur.fetchall()
 
     for row in rows:
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
     cur.close()
     connect.close()
