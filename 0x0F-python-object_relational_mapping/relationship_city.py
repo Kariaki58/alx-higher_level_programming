0e75e1ea420211aa6545e3f526ad27d45c1c6593
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-18 19:10:05 +0100

    testing solution

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import relationship
 
 
 class City(Base):
+    """city class for relationship"""
     __tablename__ = "cities"
     id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
