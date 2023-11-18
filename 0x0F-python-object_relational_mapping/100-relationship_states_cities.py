a4b7f49ed41d6216fff1bb3a9d785a1a72016d78
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-18 21:34:39 +0100

    coder

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                                                  sys.argv[2],
                                                  sys.argv[3]),
         echo=False)
-    Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
