38a4d2a9aa426ac1a9c59104fd7b874567fb1245
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-18 20:17:33 +0100

    relationship goal

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""list relationship"""
+"""model city fetch state"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,19 +8,21 @@ from relationship_city import City
 
 
 if __name__ == "__main__":
+    """entry point"""
     engine = create_engine(
         'mysql://{}:{}@localhost:3306/{}'.format(sys.argv[1],
                                                  sys.argv[2],
                                                  sys.argv[3]),
         echo=False)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(engine)
     session = Session()
-    query = session.query(State).outerjoin(City).order_by(
-        State.id, City.id
-        ).all()
-    for d1 in query:
-        print(f"{d1.id}: {d1.name}")
-        for data in d1.cities:
-                print(f"    {data.id}: {data.name}")
-    session.close() 
+
+    query = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+    
+    for data in query:
+        print(f"{data.id}: {data.name}")
+        for content in data.cities:
+            print(f"    {content.id}: {content.name}")
+    session.commit()
+    session.close()
