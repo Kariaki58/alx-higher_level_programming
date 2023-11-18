60cc271637c27468aca023f35349417bf2b7ee1f
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-18 19:50:24 +0100

    List relationship

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""list relationship"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    """entry point"""
+    engine = create_engine(
+        'mysql://{}:{}@localhost:3306/{}'.format(sys.argv[1],
+                                                 sys.argv[2],
+                                                 sys.argv[3]),
+        echo=False)
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(engine)
+    session = Session()
+
+    query_data = session.query(State).all()
+    query_next = session.query(State, City).join(City, State.id == City.state_id).all()
+
+    
+    for d1 in query_data:
+        print(f"{d1.id}: {d1.name}")
+        for _, data in query_next:
+            if d1.id == data.state_id:
+                print(f"    {data.id}: {data.name}")
+
+
+    session.commit()
+    session.close() 
