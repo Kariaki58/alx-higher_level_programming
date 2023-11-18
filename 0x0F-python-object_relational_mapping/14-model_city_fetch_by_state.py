f266af6d8f82041cd58a219b3d8c7efd372388a4
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-18 16:46:39 +0100

    modified solution

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
                                                  sys.argv[2],
                                                  sys.argv[3]),
         echo=False)
+    Base.metadata.create_all(bind=engine)
     Session = sessionmaker(engine)
     session = Session()
 
