bb61fdc8137cb8062ec4f66a1f9458f5b9934a30
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-24 14:47:03 +0100

    fixed pycodestyle

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -7,7 +7,13 @@ from sqlalchemy.orm import relationship, sessionmaker
 
 class City(Base):
     __tablename__ = "cities"
-    id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
+    id = Column(
+            Integer,
+            autoincrement=True,
+            unique=True,
+            nullable=False,
+            primary_key=True
+            )
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
     state = relationship("State", foreign_keys=state_id)
