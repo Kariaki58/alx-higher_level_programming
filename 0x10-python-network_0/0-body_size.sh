6c300f9677d19e3515469b45a9bb790e90779ac3
Kariaki58 <programmerjavascript901@gmail.com>
2023-12-05 17:43:21 -0800

    wc -l = 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #comment
-url=$1
-size=$(curl -sI "$url" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r\n')
-echo "$size"
+echo "$(curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r\n')"
