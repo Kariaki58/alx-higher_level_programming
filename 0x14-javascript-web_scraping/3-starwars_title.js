fab45ba0bbaad97076a1b9a894b64d932c730f98
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-03 13:11:17 +0100

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require("request");
+const id = process.argv[2]
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`
+request(url, (err, response) => {
+    if (response.statusCode == 200) {
+        const filmtitle = JSON.parse(response.body)
+        console.log(filmtitle['title'])
+    }
+})
