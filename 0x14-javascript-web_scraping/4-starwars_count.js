041a0d2b481c9ad2547cbdbaadb3596c79e677f2
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-03 13:32:36 +0100

    starwars count using api and character ID 18

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+
+const request = require("request");
+const url = "https://swapi-api.alx-tools.com/api/films/"
+const character_url = "https://swapi-api.alx-tools.com/api/people/18/"
+
+
+request(url, (err, respose) => {
+    if (respose.statusCode == 200) {
+        const body = JSON.parse(respose.body);
+        const loopback = body.results;
+        let count = 0;
+        for (let i = 0; i < loopback.length; i++) {
+            for (let j = 0; j < loopback[i].characters.length; j++) {
+                if (loopback[i].characters[j] == character_url) {
+                    count += 1
+                }
+            }
+        }
+        console.log(count)
+    }
+});
