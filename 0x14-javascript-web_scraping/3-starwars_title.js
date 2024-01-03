6a18f03b3cdeb2c1c74754b7c7880b4950e5daec
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-03 15:17:27 +0100

    handled error

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,8 +4,10 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 request(url, (err, response) => {
-  if (response.statusCode == 200) {
+  if (response.statusCode === 200) {
     const filmtitle = JSON.parse(response.body);
     console.log(filmtitle.title);
+  } else {
+    console.log(err);
   }
 });
