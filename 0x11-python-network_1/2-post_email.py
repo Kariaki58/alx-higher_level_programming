b68670a6c37f4d3c72b103a12ffe0e1d79420ccf
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-17 15:56:47 +0100

    post an email to a web server

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+response header value #0
+"""
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+import sys
+
+
+if __name__ == "__main__":
+    data = {'email': sys.argv[2]}
+    data = urlencode(data).encode('utf-8')
+    P_request = Request(sys.argv[1], data=data, method='POST')
+    with urlopen(P_request) as response:
+        print(f"Your email is: {response.read().decode('utf-8')}")
